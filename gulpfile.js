5a36e0c1da9dfa881449304c70c1e9eb46727831
Greg Block <gblock0@gmail.com>
2020-03-03 13:46:02 -0600

    Update gulp file and ignored files in firebase

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
-const { series } = require("gulp");
+const { series, parallel, src, dest } = require("gulp");
+const del = require("del");
+const uglifyCss = require("gulp-uglifycss");
+const CacheBuster = require("gulp-cachebust");
+var cacheBust = new CacheBuster();
 
-function css(cb) {
-  // body omitted
+async function clean(cb) {
+  await del("public");
   cb();
 }
-exports.default = series(css);
+
+function css() {
+  return src("src/**/*.css")
+    .pipe(uglifyCss())
+    .pipe(cacheBust.resources())
+    .pipe(dest("public/"));
+}
+
+function images() {
+  return src("src/**/*.png").pipe(dest("public/imgs/"));
+}
+
+function html() {
+  return src("src/**/*.html")
+    .pipe(cacheBust.references())
+    .pipe(dest("public/"));
+}
+
+exports.default = series(clean, css, parallel(images, html));
